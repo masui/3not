949aa4e75f538c6fd90128a5cd3e95e9e609013f
Toshiyuki Masui <masui@pitecan.com>
2020-10-29 11:47:15 +0900

    解けた!

--- 3not.rb
+++ 3not.rb
@@ -1,11 +1,4 @@
 
-
-d = {}
-d[0b01010101] = '01010101'
-d[0b00110011] = '00110011'
-d[0b00001111] = '00001111'
-
-
 def check(d)
   found2 = true
   while found2 do
@@ -42,9 +35,16 @@ def check(d)
   d
 end
 
-d.keys.each { |v|
-  d2 = d.dup
-  d2[v ^ 0b11111111] = "(^ #{d[v]})"
+d1 = {}
+d1[0b01010101] = '01010101'
+d1[0b00110011] = '00110011'
+d1[0b00001111] = '00001111'
+
+d1 = check(d1)
+
+d1.keys.each { |v|
+  d2 = d1.dup
+  d2[v ^ 0b11111111] = "(^ #{d1[v]})"
   d2 = check(d2)
   d2.keys.each { |v|
     d3 = d2.dup
@@ -52,50 +52,15 @@ d.keys.each { |v|
     d3 = check(d3)
     if d3[0b10101010] && d3[0b11001100] && d3[0b11110000]
       puts "FOUND!!!!"
+      puts "10101010 = #{d3[0b10101010]}"
+      puts "11001100 = #{d3[0b11001100]}"
+      puts "11110000 = #{d3[0b11110000]}"
+      exit
     end
   }
 }
 
 
-
-# count = 0
-# 
-# d = [0b01010101, 0b00110011,0b00001111].sort.uniq
-# 
-# while true do
-#   while true do
-#     oldd = d.dup
-#     oldd.combination(2) { |a|
-#       d.push (a[0] & a[1])
-#       d = d.sort.uniq
-#     }
-#     break if d.length == oldd.length
-#   end
-#   
-#   while true do
-#     oldd = d.dup
-#     oldd.combination(2) { |a|
-#       d.push (a[0] | a[1])
-#       d = d.sort.uniq
-#     }
-#     break if d.length == oldd.length
-#   end
-#   
-#   exit if d.member?(0b10101010) && d.member?(0b11001100) && d.member?(0b11110000)
-#   
-#   d.each { |v|
-#     b = sprintf("%08b",v)
-#     puts b
-#   }
-#   exit
-# 
-#   # not追加
-#   d.dup.each { |v|
-#     d.push (v ^ 0b11111111)
-#   }
-#   d = d.sort.uniq
-# 
-# end
 
 #while true do
 #  現状の集合すべての組み合わせについてandを計算したのを追加
