b377fb4b2c2b565345362e638876f0b590b258c6
Toshiyuki Masui <masui@pitecan.com>
2020-10-29 18:50:19 +0900

    ...

--- 3not.rb
+++ 3not.rb
@@ -1,35 +1,31 @@
 
 def check(d)
-  found2 = true
-  while found2 do
-    found2 = false
-    found = true
-    while found do
-      found = false
-      d_save = d.dup
-      d_save.keys.combination(2){ |a|
+  while true do
+    while true do
+      found_and = false
+      d.keys.combination(2){ |a|
         v = a[0] & a[1]
         unless d[v]
           d[v] = "(#{d[a[0]]} & #{d[a[1]]})"
-          found = true
-          found2 = true
+          found_and = true
         end
       }
+      break unless found_and
     end
     
-    found = true
-    while found do
-      found = false
-      d_save = d.dup
-      d_save.keys.combination(2){ |a|
+    while true do
+      found_or = false
+      d.keys.combination(2){ |a|
         v = a[0] | a[1]
         unless d[v]
           d[v] = "(#{d[a[0]]} | #{d[a[1]]})"
-          found = true
-          found2 = true
+          found_or = true
         end
       }
+      break unless found_or
     end
+
+    break unless found_and || found_or
   end
 
   d
@@ -39,6 +35,9 @@ d1 = {}
 d1[0b01010101] = '0b01010101'
 d1[0b00110011] = '0b00110011'
 d1[0b00001111] = '0b00001111'
+d1[0b01010101] = 'A'
+d1[0b00110011] = 'B'
+d1[0b00001111] = 'C'
 
 d1 = check(d1)
 
@@ -55,18 +54,22 @@ d1.keys.each { |v|
 def inv(v)
   0b11111111 ^ v
 end
+
+A = 0b01010101
+B = 0b00110011
+C = 0b00001111
+
+puts '10101010'
+puts sprintf('%08b',#{d3[0b10101010]})
+puts '11001100'
+puts sprintf('%08b',#{d3[0b11001100]})
+puts '11110000'
+puts sprintf('%08b',#{d3[0b11110000]})
 EOF
-      puts "puts '10101010'"
-      puts "puts sprintf('%08b',#{d3[0b10101010]})"
-      puts "puts '11001100'"
-      puts "puts sprintf('%08b',#{d3[0b11001100]})"
-      puts "puts '11110000'"
-      puts "puts sprintf('%08b',#{d3[0b11110000]})"
       exit
     end
   }
 }
-
 
 
 #while true do
@@ -77,6 +80,4 @@ EOF
 #   end
 #   break if 3つのnotが出現してる
 #   現状の集合すべての組み合わせについてnotを計算したのを追加
-#   count += 1
 # end
-# puts count
