dc62171ef677dda749eca4632db4ef8e5e40000b
Toshiyuki Masui <masui@pitecan.com>
2020-10-29 12:00:46 +0900

    Rubyプログラムを出力

--- 3not.rb
+++ 3not.rb
@@ -36,25 +36,32 @@ def check(d)
 end
 
 d1 = {}
-d1[0b01010101] = '01010101'
-d1[0b00110011] = '00110011'
-d1[0b00001111] = '00001111'
+d1[0b01010101] = '0b01010101'
+d1[0b00110011] = '0b00110011'
+d1[0b00001111] = '0b00001111'
 
 d1 = check(d1)
 
 d1.keys.each { |v|
   d2 = d1.dup
-  d2[v ^ 0b11111111] = "(^ #{d1[v]})"
+  d2[v ^ 0b11111111] = "(inv(#{d1[v]}))"
   d2 = check(d2)
   d2.keys.each { |v|
     d3 = d2.dup
-    d3[v ^ 0b11111111] = "(^ #{d2[v]})"
+    d3[v ^ 0b11111111] = "(inv(#{d2[v]}))"
     d3 = check(d3)
     if d3[0b10101010] && d3[0b11001100] && d3[0b11110000]
-      puts "FOUND!!!!"
-      puts "10101010 = #{d3[0b10101010]}"
-      puts "11001100 = #{d3[0b11001100]}"
-      puts "11110000 = #{d3[0b11110000]}"
+      print <<EOF
+def inv(v)
+  0b11111111 ^ v
+end
+EOF
+      puts "puts '10101010'"
+      puts "puts sprintf('%08b',#{d3[0b10101010]})"
+      puts "puts '11001100'"
+      puts "puts sprintf('%08b',#{d3[0b11001100]})"
+      puts "puts '11110000'"
+      puts "puts sprintf('%08b',#{d3[0b11110000]})"
       exit
     end
   }
